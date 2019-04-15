cbc4a03c406f04aac7df9f660836503b2cf3ca10
HouariZEGAI <houarizegai14@gmail.com>
2019-04-15 21:45:16 +0100

    Adding email information - constants

---
+++ src/com/houarizegai/spygen/global/Constants.java
@@ -0,0 +1,13 @@
+package com.houarizegai.spygen.global;
+
+public class Constants {
+    public static final String senderMail;
+    public static final String senderPassword;
+    public static final String receiverMail;
+
+    static {
+        senderMail = "senderMail@gmail.com";
+        senderPassword = "yourPassword";
+        receiverMail = "receiverMail@gmail.com";
+    }
+}
